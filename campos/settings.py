c0009c891e6da8886bcd9987043af9da334b412b
casper <ahmadsy88@gmail.com>
2022-08-21 15:41:02 +0300

    feat: settings

--- campos/settings.py
+++ campos/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'articles',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = '/'
+
--- campos/urls.py
+++ campos/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.http import HttpResponse
+from django.shortcuts import render
+from accounts.views import login_view, logout_view
 
 urlpatterns = [
+    path('', login_view, name='login'),
+    path('', logout_view, name='logout'),
     path('admin/', admin.site.urls),
+    path('articles/', include('articles.urls')),
+
 ]
